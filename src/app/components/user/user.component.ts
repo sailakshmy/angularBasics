532bff4b704a416e7a4a96d2e47f85fde54bb1af
Sai <sailakshmy94@gmail.com>
2025-07-11 08:24:36 +0400

    Stored the data in the user component

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../../dummy-data/dummy-users';
 
+const randomIndex = Math.floor(Math.random()* DUMMY_USERS.length)
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -7,6 +9,8 @@ import { Component } from '@angular/core';
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {
 
+
+export class UserComponent {
+  selectedUser = DUMMY_USERS[randomIndex]
 }
