d33bb6966e6a099f3196da321c7ac85b4695ddb3
Sai <sailakshmy94@gmail.com>
2025-07-11 12:04:35 +0400

    Defined Component Inputs

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,27 +1,34 @@
-import { Component,} from '@angular/core';
+import { Component, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-user',
+  selector: "app-user",
   standalone: true,
   imports: [],
-  templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  templateUrl: "./user.component.html",
+  styleUrl: "./user.component.css",
 })
-
-
 export class UserComponent {
+  @Input() avatar!: string;
+  @Input() name!: string;
+
+  get imagePath() {
+    return `assets/users/${this.avatar}`;
+  }
+
+  onSelectPath(){}
+
   // selectedUser = DUMMY_USERS[randomIndex];
   // selectedUser = signal(DUMMY_USERS[randomIndex])
   // imagePath = computed(()=>`assets/users/${this.selectedUser().avatar}`)
-  
+
   // get imagePath(){
-    // return `assets/users/${this.selectedUser.avatar}`
+  // return `assets/users/${this.selectedUser.avatar}`
   // }
 
-  onSelectUser(){
-    // console.log("clicked");
-    // const randomIndex = Math.floor(Math.random()* DUMMY_USERS.length)
-    // // this.selectedUser = DUMMY_USERS[randomIndex];
-    // this.selectedUser.set(DUMMY_USERS[randomIndex])
-  }
+  // onSelectUser(){
+  // console.log("clicked");
+  // const randomIndex = Math.floor(Math.random()* DUMMY_USERS.length)
+  // // this.selectedUser = DUMMY_USERS[randomIndex];
+  // this.selectedUser.set(DUMMY_USERS[randomIndex])
+  // }
 }
