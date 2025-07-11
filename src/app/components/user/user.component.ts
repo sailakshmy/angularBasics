8ebc40ea4f997fb78296908fc1b4bd1d4ceba1a9
Sai <sailakshmy94@gmail.com>
2025-07-11 12:07:30 +0400

    Configured required and optional inputs

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -8,14 +8,17 @@ import { Component, Input } from "@angular/core";
   styleUrl: "./user.component.css",
 })
 export class UserComponent {
-  @Input() avatar!: string;
-  @Input() name!: string;
+  @Input({
+    required: true,
+  })
+  avatar!: string;
+  @Input({ required: true }) name!: string;
 
   get imagePath() {
     return `assets/users/${this.avatar}`;
   }
 
-  onSelectPath(){}
+  onSelectPath() {}
 
   // selectedUser = DUMMY_USERS[randomIndex];
   // selectedUser = signal(DUMMY_USERS[randomIndex])
