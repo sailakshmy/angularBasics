ff2ec8f35d7d8a624ef4481a603b1c6a6e8e32f4
Sai <sailakshmy94@gmail.com>
2025-07-12 18:18:05 +0400

    Fixed the completeTask functionality using services

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -24,8 +24,6 @@ export class TasksComponent {
     return this.tasksService.getUserTasks(this.userId);
   }
 
-  onCompleteTask(taskId: string) {}
-
   onStartAddTask() {
     this.isAddingTask = true;
   }
