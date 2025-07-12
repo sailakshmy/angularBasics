9c2319bd66e1bb964874fa0a7e519cf1c31dcd11
Sai <sailakshmy94@gmail.com>
2025-07-12 11:23:56 +0400

    Deleted tasks programmatically

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -9,12 +9,6 @@ import {
 } from "@angular/core";
 import { User } from "./user.model";
 
-// type User = {
-//   id: string;
-//   name: string;
-//   avatar: string;
-// };
-
 @Component({
   selector: "app-user",
   standalone: true,
@@ -25,47 +19,51 @@ import { User } from "./user.model";
 export class UserComponent {
   @Input({ required: true }) user!: User;
   @Input({ required: true }) selected!: boolean;
-  // @Input({
-  //   required: true,
-  // })
-  // avatar!: string;
-  // @Input({ required: true }) name!: string;
-  // @Input({ required: true }) id!: string;
+
   @Output()
   select = new EventEmitter<string>();
-  // select = output<string>();
 
   get imagePath() {
     return `assets/users/${this.user.avatar}`;
-    // return `assets/users/${this.avatar}`;
   }
   onSelectUser() {
     this.select.emit(this.user.id);
-    // this.select.emit(this.id);
   }
-  // avatar = input.required<string>();
-  // name = input.required<string>();
-  // imagePath = computed(() => `assets/users/${this.avatar()}`);
-
-  // @Input({
-  //   required: true,
-  // })
-  // avatar!: string;
-  // @Input({ required: true }) name!: string;
-  // get imagePath() {
-  //   return `assets/users/${this.avatar}`;
-  // }
-  // onSelectUser() {}
-  // selectedUser = DUMMY_USERS[randomIndex];
-  // selectedUser = signal(DUMMY_USERS[randomIndex])
-  // imagePath = computed(()=>`assets/users/${this.selectedUser().avatar}`)
-  // get imagePath(){
-  // return `assets/users/${this.selectedUser.avatar}`
-  // }
-  // onSelectUser(){
-  // console.log("clicked");
-  // const randomIndex = Math.floor(Math.random()* DUMMY_USERS.length)
-  // // this.selectedUser = DUMMY_USERS[randomIndex];
-  // this.selectedUser.set(DUMMY_USERS[randomIndex])
-  // }
 }
+
+// type User = {
+//   id: string;
+//   name: string;
+//   avatar: string;
+// };
+// @Input({
+//   required: true,
+// })
+// avatar!: string;
+// @Input({ required: true }) name!: string;
+// @Input({ required: true }) id!: string;
+// avatar = input.required<string>();
+// name = input.required<string>();
+// imagePath = computed(() => `assets/users/${this.avatar()}`);
+
+// @Input({
+//   required: true,
+// })
+// avatar!: string;
+// @Input({ required: true }) name!: string;
+// get imagePath() {
+//   return `assets/users/${this.avatar}`;
+// }
+// onSelectUser() {}
+// selectedUser = DUMMY_USERS[randomIndex];
+// selectedUser = signal(DUMMY_USERS[randomIndex])
+// imagePath = computed(()=>`assets/users/${this.selectedUser().avatar}`)
+// get imagePath(){
+// return `assets/users/${this.selectedUser.avatar}`
+// }
+// onSelectUser(){
+// console.log("clicked");
+// const randomIndex = Math.floor(Math.random()* DUMMY_USERS.length)
+// // this.selectedUser = DUMMY_USERS[randomIndex];
+// this.selectedUser.set(DUMMY_USERS[randomIndex])
+// }
